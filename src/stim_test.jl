2cbe47df02745db604a583b33156e47cce86f6e8
Madhav Krishnan Vijayan <39261345+madhavkrishnan@users.noreply.github.com>
2021-04-08 10:42:59 +1000

    Add graph plotting to test file

--- src/stim_test.jl
+++ src/stim_test.jl
@@ -5,9 +5,11 @@ state.simulator.h(0)
 state.simulator.cnot(0, 1)
 
 Jabalizer.update_tableau(state)
-
-graph_state = Jabalizer.ToGraph(state)
-
-
 print(state)
+
+graph_state, adj, local_ops = Jabalizer.ToGraph(state)
 print(graph_state)
+
+print(graph_state.simulator.current_inverse_tableau()^-1)
+
+Jabalizer.gplot(graph_state)
