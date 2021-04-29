f3103ccdaf6ea28056a6cce4d9f94ab0071752c2
Madhav Krishnan Vijayan <39261345+madhavkrishnan@users.noreply.github.com>
2021-04-29 12:27:38 +1000

    Implement correct gates to perform X and Y measurements

--- src/stabilizer_measurements.jl
+++ src/stabilizer_measurements.jl
@@ -31,14 +31,29 @@ function MeasureZ(state::StabilizerState, qubit::Int64)::Int64
 end
 
 function MeasureX(state::StabilizerState, qubit::Int64)::Int64
+    # Convert to |+>, |-> basis
     H(state, qubit)
+    # Measure along z (now x)
     outcome = MeasureZ(state, qubit)
+    # Return to computational basis
+    H(state, qubit)
+
     return(outcome)
 end
 
 function MeasureY(state::StabilizerState, qubit::Int64)::Int64
+
+    # Map Y eigenstates to X eigenstates
+    # Note that P^dagger = ZP
+    Z(state, qubit)
+    P(state, qubit)
+    H(state, qubit)
+    # Measure along z (now y)
+    outcome = MeasureZ(state, qubit)
+
+    # Return to original basis
     H(state, qubit)
     P(state, qubit)
-    outcome = MeasureZ(state, qubit)
+
     return(outcome)
 end
--- src/stim_test.jl
+++ src/stim_test.jl
@@ -1,9 +1,6 @@
 include("jabalizer.jl")
 
-
 state = Jabalizer.StabilizerState()
-#state.simulator.h(0)
-#state.simulator.cnot(0, 1)
 
 # GHZ state test
 Jabalizer.H(state, 1)
@@ -21,6 +18,7 @@ Jabalizer.CNOT(state, 2,3)
 # Jabalizer.SWAP(state, 3, 4)
 # Jabalizer.CNOT(state, 2, 4)
 
+# Update simulator results into the state tableau
 Jabalizer.update_tableau(state)
 
 println()
@@ -28,18 +26,23 @@ println("State before measurement")
 println()
 print(state)
 
+# # Apply measurements
 
-# z = Jabalizer.MeasureZ(state, 1)
-# x = Jabalizer.MeasureX(state, 4)
-y = Jabalizer.MeasureY(state, 1) #This will cause an error
-# y = Jabalizer.MeasureY(state, 5)
+# mz_qubit = 1
+# z = Jabalizer.MeasureZ(state, mz_qubit)
+# println("", "MeasureY on qubit $mz_qubit, Result: ", z, "\n")
 
-
+# mx_qubit = 3
+# x = Jabalizer.MeasureX(state, mx_qubit)
+# println("", "MeasureX on qubit $mx_qubit, Result: ", x, "\n")
 #
-# println("", "MeasureZ : ", z, "\n")
-# println("", "MeasureX : ", x, "\n")
-println("", "MeasureY : ", y, "\n")
 
+# my_qubit = 5 #This will cause an error (for all gates test)
+my_qubit = 1
+y = Jabalizer.MeasureY(state, my_qubit)
+println("", "MeasureY on qubit $my_qubit, Result:", y, "\n")
+
+# Update simulator results into the state tableau
 Jabalizer.update_tableau(state)
 
 println("State after measurement")
