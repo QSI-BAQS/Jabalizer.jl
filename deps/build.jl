ba445870cd7cb2c5fd602de689c88e939c2317c8
Madhav Krishnan Vijayan <39261345+madhavkrishnan@users.noreply.github.com>
2021-09-21 17:42:51 +1000

    Force PyCall to use the Conda python installation.

--- deps/build.jl
+++ deps/build.jl
@@ -5,3 +5,7 @@ Pkg.add(PackageSpec(name="Conda", rev="master"))
 using Conda
 Conda.pip_interop(true)
 Conda.pip("install", "stim>=1.2.1")
+
+# Force PyCall to use local python distribution
+ENV["PYTHON"] = ""
+Pkg.build("PyCall")
