5d6460c11829af5340d44f5c17012a1dd5515c4a
Madhav Krishnan Vijayan <39261345+madhavkrishnan@users.noreply.github.com>
2022-04-12 13:00:29 +1000

    Add cirq dependency duiring build

--- deps/build.jl
+++ deps/build.jl
@@ -5,6 +5,7 @@ Pkg.add(PackageSpec(name="Conda", rev="master"))
 using Conda
 Conda.pip_interop(true)
 Conda.pip("install", "stim>=1.2.1")
+Conda.pip("install", "cirq")
 
 # Force PyCall to use Julia specific python distribution
 ENV["PYTHON"] = ""
