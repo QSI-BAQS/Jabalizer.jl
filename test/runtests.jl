3f50044d3743dcba75ed45ceff305488b22537f1
mstechly <michal.stechly@gmail.com>
2022-06-21 11:29:46 -0400

    Fix runtests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,8 @@
 using Jabalizer
-# using PyCall
 using Test
 
-include("setup_test.jl")
+# include("setup_test.jl")
 
-# include("old_tests.jl")
+include("old_tests.jl")
 
-include("end_to_end.jl")
+# include("end_to_end.jl")
