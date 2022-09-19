b65f5efd1cd3550832d7c9aab5dfb67f27fc143f
Madhav Krishnan Vijayan <39261345+madhavkrishnan@users.noreply.github.com>
2022-09-19 15:44:56 +1000

    Update demo file

--- examples/demo.jl
+++ examples/demo.jl
@@ -1,21 +1,26 @@
 using Jabalizer
-# using Graphs, GraphPlot
 
 # Prepare a 6-qubit GHZ state
 n = 6
 state = zero_state(n)
-state |> H(1) |> CNOT(1,2) |> CNOT(1,3) |> CNOT(1,4) |> CNOT(1,5) |> CNOT(1,6)
-# Display the stabilizer tableau
-tab = to_tableau(state)
-display(tab)
+println("\nInitial State")
+print(state)
+
+state |> Jabalizer.H(1) |> Jabalizer.CNOT(1,2) |> Jabalizer.CNOT(1,3) |> Jabalizer.CNOT(1,4) |> Jabalizer.CNOT(1,5) |> Jabalizer.CNOT(1,6)
+
+println("\nGHZ state")
+print(state)
 
 # Convert to graph state
 graph_state = GraphState(state)
-println("Graph adjacency matrix")
+println("\nGraph adjacency matrix")
 display(graph_state.A)
-gplot(Graph(graph_state.A))
+
+# This requires some kind of plotting backend.
+# Tested with plot pane in Atom.
+display(Jabalizer.gplot(graph_state))
 
 # Convert back to stabilizer state
 stab_state = StabilizerState(graph_state)
-tab = to_tableau(stab_state)
-display(tab)
+println("\nStabilizer State")
+print(stab_state)
