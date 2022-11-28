46a0cf18510038402f18ecfceae75de4860517c4
ScottPJones <scottjones@alum.mit.edu>
2022-11-28 12:32:04 -0500

    debugging

--- src/util.jl
+++ src/util.jl
@@ -40,6 +40,32 @@ function stim_tableau(stim_sim::Py)::Matrix{Int}
     return tableau
 end
 
+function clear!(stab::Stabilizer)
+    fill!(stab.X.chunks, 0)
+    fill!(stab.Z.chunks, 0)
+    stab
+end
+
+function copystate!(stab::Stabilizer, tableau, row, qubits)
+    ps = tableau.z_output(row - 1)
+    # update sign
+    stab.phase = pyconvert(Number, ps.sign) == -1 ? 2 : 0
+
+    # Stabilizer replacement
+    for j in 1:qubits
+        v = pyconvert(Int, get(ps, j - 1, -1))
+        # Note: 0123 represent IXYZ (different from the xz representation used here)
+        if v == 1           # X replacement
+            stab.X[j] = 1
+        elseif v == 3       # Z replacement
+            stab.Z[j] = 1
+        elseif v == 2       # Y replacement
+            stab.X[j] = 1
+            stab.Z[j] = 1
+        end
+    end
+end
+
 """
     update_tableau(state::StabilizerState)
 
@@ -56,32 +82,14 @@ function n_update_tableau(state::StabilizerState)
     #qubits == len || error("Mismatch qubits $qubits != $len")
     svec = state.stabilizers
     if isempty(svec)
-        state.stabilizers = svec = [Stabilizer(qubits) for i = 1:qubits]
-    end
-
-    for i in 1:qubits
-        stab = svec[i]
-        ps = forward_tableau.z_output(i - 1)
-        # update sign
-        stab.phase = pyconvert(Number, ps.sign) == -1 ? 2 : 0
-
-        # Stabilizer replacement
-        for j in 1:qubits
-            v = pyconvert(Int, get(ps, j - 1, -1))
-            # Note: 0123 represent IXYZ (different from the xz representation used here)
-            if v == 1           # X replacement
-                stab.X[j] = 1
-                stab.Z[j] = 0
-            elseif v == 3       # Z replacement
-                stab.X[j] = 0
-                stab.Z[j] = 1
-            elseif v == 2       # Y replacement
-                stab.X[j] = 1
-                stab.Z[j] = 1
-            else
-                stab.X[j] = 0
-                stab.Z[j] = 0
-            end
+        for i in 1:qubits
+            stab = Stabilizer(qubits)
+            push!(svec, stab)
+            copystate!(stab, forward_tableau, i, qubits)
+        end
+    else
+        for i in 1:qubits
+            copystate!(clear!(svec[i]), forward_tableau, i, qubits)
         end
     end
 
@@ -103,23 +111,7 @@ function Base.rand(::Type{StabilizerState}, qubits::Int)
     for i in 1:qubits
         stab = Stabilizer(qubits)
         push!(svec, stab)
-        ps = forward_tableau.z_output(i - 1)
-        # update sign
-        stab.phase = pyconvert(Number, ps.sign) == -1 ? 2 : 0
-
-        # Stabilizer replacement
-        for j in 1:qubits
-            v = pyconvert(Int, get(ps, j - 1, -1))
-            # Note: 0123 represent IXYZ (different from the xz representation used here)
-            if v == 1           # X replacement
-                stab.X[j] = 1
-            elseif v == 3       # Z replacement
-                stab.Z[j] = 1
-            elseif v == 2       # Y replacement
-                stab.X[j] = 1
-                stab.Z[j] = 1
-            end
-        end
+        copystate!(stab, forward_tableau, i, qubits)
     end
 
     # mark it as updated
@@ -168,85 +160,80 @@ end
 
 Convert a state to its adjacency graph state equivalent under local operations.
 """
-function to_graph(state::StabilizerState)
+function _to_graph(state::StabilizerState)
 
     #TODO: Add a check if state is not empty. If it is, throw an exception.
     # update the state tableau from the stim simulator
-    @timeit to "update_tableau" update_tableau(state)
+    print("update_tableau: "); @time update_tableau(state)
     qubits = state.qubits
     svec = deepcopy(state.stabilizers)
     # Sequence of local operations performed
     op_seq = Tuple{String, Int}[]
-    #@timeit to "sort" sort!(svec, rev=true)
-    @time sort!(svec, rev=true)
-    #=
-    debug_graph && (savesvec = deepcopy(svec))
-    # check if any rows changed
-    debug_graph && svec != savesvec && disp_diff("sort", svec, savesvec, 1, qubits)
-    =#
+    print("sort!: "); @time sort!(svec, rev=true)
 
     # Make X-block upper triangular
     for n in 1:qubits
-        #@timeit to "calc_sum 1" (lead_sum = calc_sum(svec, qubits, n))
-        #@time (lead_sum = calc_sum(svec, qubits, n))
-        lead_sum = calc_sum(svec, qubits, n)
+        # Find first X (or Y) below diagonal.
+        first_x = find_first_x(svec, qubits, n, n)
 
-        if lead_sum == 0
-            # Perform Hadamard operation
-            println("Hadamard: ", n)
-            @timeit to "hadamard" begin
-            for stab in svec
-                x, z = stab.X[n], stab.Z[n]
-                if xor(x, z) == 1
-                    # Swap bits
-                    stab.X[n], stab.Z[n] = z, x
-                elseif x == 1 && z == 1
-                    stab.phase ⊻= 2 # toggle bit 2 of phase if Y
+        # if diagonal is zero,
+        #    1) perform Hadamard operation if no other X found
+        #    2) swap rows with first X found
+        if svec[n].X[n] == 0
+            if first_x == 0
+                # Perform Hadamard operation
+                @time begin
+                    for stab in svec
+                        x, z = stab.X[n], stab.Z[n]
+                        if xor(x, z) == 1
+                            # Swap bits
+                            stab.X[n], stab.Z[n] = z, x
+                        elseif x == 1 && z == 1
+                            stab.phase ⊻= 2 # toggle bit 2 of phase if Y
+                        end
+                    end
+                    push!(op_seq, ("H", n))
+                    # Recalculate first_x (should always be non-zero,
+                    # since Z column should have at least 1 bit set
+                    first_x = find_first_x(svec, qubits, n, n)
+                    print("Hadamard: $n -> $first_x, ")
                 end
             end
-            push!(op_seq, ("H", n))
+            # If we are not already at end (i.e. n == qubits), swap rows
+            if first_x != 0
+                # Swap rows to bring X to diagonal
+                svec[n], svec[first_x] = svec[first_x], svec[n]
+                print("Swap: $n, $first_x -> ")
+                # Recalculate first_x after swap, starting after first_x row (which now has 0 X)
+                first_x = find_first_x(svec, qubits, first_x, n)
+                println(first_x)
             end
-            #debug_graph && (savesvec = deepcopy(svec))
-            #@timeit to "sort H" sort!(svec, rev=true)
-            @time sort!(view(svec, n, rev=true)
-            #=
-            if debug_graph
-                if svec == savesvec
-                    println("sort H - no change")
-                else
-                    disp_diff("sort H", svec, savesvec, n, qubits)
-                end
-            end
-            =#
-            #@timeit to "calc_sum 2" lead_sum = calc_sum(svec, qubits, n)
-            #@time (lead_sum = calc_sum(svec, qubits, n))
-            lead_sum = calc_sum(svec, qubits, n))
         end
 
-        if lead_sum > 1
-            println("add rows(", lead_sum, "): ", n+1, ":", n+lead_sum-1)
-            #@timeit to "add row loop" begin
-            print("    ")
+        # If there are any rows with X set in this column below the diagonal,
+        # perform rowadd operations
+        if first_x != 0
+            print("add rows: $n, $first_x\t")
             @time begin
-            for m in (n+1):(n+lead_sum-1)
-                _add_row!(svec, n, m)
+                stab = svec[n]
+                for m in first_x:qubits
+                    stab.X[m] == 0 || _add_row!(svec, n, m)
+                end
             end
-            end
-            # debug_graph && (savesvec = deepcopy(svec))
-            #@timeit to "sort 2" sort!(svec, rev=true)
-            print("\n    sort")
-            @time sort!(svec, rev=true)
-            # check if any rows changed
-            # debug_graph && svec != savesvec && disp_diff("sort", svec, savesvec, n, qubits)
         end
     end
+    show(svec)
+    println()
 
     # Make diagonal X-block
-    @timeit to "make diagonal" begin
+    print("Make diagonal: ")
+    @time begin
     for n = (qubits-1):-1:1, m = (n+1):qubits
-        svec[n].X[m] == 1 && _add_row!(svec, m, n)
+        svec[n].X[m] == 0 && _add_row!(svec, m, n)
     end
     end
+    show(svec)
+    println()
 
     # Adjacency matrix
     A = Array{Int}(undef, qubits, qubits)
@@ -288,8 +275,11 @@ function to_graph(state::StabilizerState)
     end
     show(to)
     println()
-    return (graph_to_state(A), A, op_seq)
+#   return (graph_to_state(A), A, op_seq)
+    return (svec, graph_to_state(A), A, op_seq)
 end
+
+to_graph(state::StabilizerState) = _to_graph(state)[2:4]
 
 function to_graph_1(state::StabilizerState)
 
@@ -335,6 +325,8 @@ function to_graph_1(state::StabilizerState)
                 end
             end
             @timeit to "calc_sum 2" lead_sum = calc_sum(svec, qubits, n)
+            show(svec)
+            println()
         end
 
         if lead_sum > 1
@@ -348,6 +340,8 @@ function to_graph_1(state::StabilizerState)
             @timeit to "sort 2" sort!(svec, rev=true)
             # check if any rows changed
             debug_graph && svec != savesvec && disp_diff("sort", svec, savesvec, n, qubits)
+            show(svec)
+            println()
         end
     end
 
@@ -398,7 +392,8 @@ function to_graph_1(state::StabilizerState)
     end
     show(to)
     println()
-    return (graph_to_state(A), A, op_seq)
+    #    return (graph_to_state(A), A, op_seq)
+    return (svec, graph_to_state(A), A, op_seq)
 end
 
 """Calculate number of X bits set in column bit"""
@@ -410,11 +405,12 @@ function calc_sum(svec, qubits, bit)
     tot
 end
 
-"""Find first X set below the diagonal in column bit"""
-function find_first_x(svec, qubits, bit)
-    i = bit
-    while (i += 1) <= qubits && svec[i].X[bit] == 0 ; end
-    return i
+"""Find first X set below the diagonal in column col"""
+function find_first_x(svec, qubits, row, col)
+    while (row += 1) <= qubits
+        svec[row].X[col] == 0 || return row
+    end
+    return 0
 end
 
 """
@@ -481,23 +477,38 @@ function old_graph(state::StabilizerState)
         lead_sum = sum(tab[n:stabs, n])
 
         if lead_sum == 0
+            println("H $n")
             hadamard!(tab, n)
             push!(LOseq, ("H", n))
             tab = sortslices(tab, dims=1, rev=true)
             lead_sum = sum(tab[n:stabs, n])
+            show(tab)
+            println()
         end
 
         if lead_sum > 1
+            print("Add row:")
             for m in (n+1):(n+lead_sum-1)
+                print(" $n:$m")
                 _add_row!(tab, n, m)
             end
+            println()
+            show(tab)
+            println()
         end
     end
 
     # Make diagonal X-block
+    print("Diagonal X-block:")
     for n = (stabs-1):-1:1, m = (n+1):stabs
-        tab[n, m] == 1 && _add_row!(tab, m, n)
+        if tab[n, m] == 1
+            print(" $m:$n")
+            _add_row!(tab, m, n)
+        end
     end
+    println()
+    show(tab)
+    println()
 
     # Phase correction
     for n = 1:qubits
@@ -527,7 +538,8 @@ function old_graph(state::StabilizerState)
     tr(A) == 0 || println("Error: invalid graph conversion (non-zero trace).")
     phases == 0 || println("Error: invalid graph conversion (non-zero phase).\nphases=$phases")
 
-    return (newState, A, LOseq)
+    #    return (newState, A, LOseq)
+    return (tab, newState, A, LOseq)
 end
 
 # TODO: Why this is the only operation we have for tabs?
