eb900249f6c157222897e778b52d28227cdea9df
ScottPJones <scottjones@alum.mit.edu>
2023-02-21 15:46:06 -0500

    Add full list of 1 and 2 gate Clifford gates that Stim supports

--- src/execute_circuit.jl
+++ src/execute_circuit.jl
@@ -5,16 +5,72 @@ const gate_map = Dict()
 function _init_gate_map()
     copy!(gate_map,
         Dict("I" => Jabalizer.Id,
-            "H" => Jabalizer.H,
-            "X" => Jabalizer.X,
-            "Y" => Jabalizer.Y,
-            "Z" => Jabalizer.Z,
-            "CNOT" => Jabalizer.CNOT,
-            "SWAP" => Jabalizer.SWAP,
-            "S" => Jabalizer.P,
-            "PHASE" => Jabalizer.P, # TODO: IS IT?!?!?!
-            "CZ" => Jabalizer.CZ))
-
+             "X" => Jabalizer.X,	# HSSH
+             "Y" => Jabalizer.Y,	# SSHSSH
+             "Z" => Jabalizer.Z,	# SS
+             "C_XYZ" => Jabalizer.C_XYZ,	# SSSH
+             "C_ZYX" => Jabalizer.C_ZYX,	# HS
+             "H" => Jabalizer.H,	# H_XZ
+             "H_XZ" => Jabalizer.H,
+             "H_XY" => Jabalizer.H_XY,		# HSSHS
+             "H_YZ" => Jabalizer.H_YZ,		# HSHSS
+             "S" => Jabalizer.P,		# SQRT_Z
+             "SQRT_X" => Jabalizer.SQRT_X,		# HSH
+             "SQRT_X_DAG" => Jabalizer.SQRT_X_DAG,	# SHS
+             "SQRT_Y" => Jabalizer.SQRT_Y,		# SSH
+             "SQRT_Y_DAG" => Jabalizer.SQRT_Y_DAG,	# HSS
+             "SQRT_Z" => Jabalizer.P,		# S
+             "SQRT_Z_DAG" => Jabalizer.SQRT_Z_DAG,	# SSS
+             "S_DAG" =>  => Jabalizer.S_DAG,		# SQRT_Z_DAG
+             "SWAP" => Jabalizer.SWAP,
+                 # CNOT a b; CNOT b a; CNOT a b
+             "CXSWAP" => Jabalizer.CXSWAP,
+                 # CNOT b a; CNOT a b
+             "PHASE" => Jabalizer.P, # TODO: IS IT?!?!?!
+             "CNOT" => Jabalizer.CNOT,	# CX,ZCX
+             "CX" => Jabalizer.CNOT,
+             "ZCX" => Jabalizer.CNOT,
+             "CY" => Jabalizer.CY,  # ZCY
+                 # S b; S b; S b; CNOT a b; S b
+             "ZCY" => Jabalizer.CY,
+             "CZ" => Jabalizer.CZ,	# ZCZ
+                 # H b; CNOT a b; H b
+             "ZCZ" => Jabalizer.CZ,
+             "ISWAP" => Jabalizer.ISWAP,
+                 # H a; S a; S a; S a; CNOT a b; S a; CNOT b a; H b; S b; S a
+             "ISWAP_DAG" => Jabalizer.ISWAP_DAG,
+                 # S a; S a; S a; S b; S b; S b; H b;
+		 # CNOT b a; S a; S a; S a;
+                 # CNOT a b; S a; H a
+	     "SQRT_XX" => Jabalizer.SQRT_XX,
+                 # H a; CNOT a b; H b; S a; S b; H a; H b
+	     "SQRT_XX_DAG" => Jabalizer.SQRT_XX_DAG,
+                 # H a; CNOT a b; H b; S a; S a; S a; S b; S b; S b; H a; H b
+	     "SQRT_YY" => Jabalizer.SQRT_YY,
+                 # S a; S a; S a; S b; S b; S b; H a;
+                 # CNOT a b; H b; S a; S b; H a; H b; S a; S b
+	     "SQRT_YY_DAG" => Jabalizer.SQRT_YY_DAG,
+                 # S a; S a; S a; S b; H a;
+                 # CNOT a b; H b; S a; S b; H a; H b; S a; S b; S b; S b;
+	     "SQRT_ZZ" => Jabalizer.SQRT_ZZ,
+                 # H b; CNOT a b; H b; S a; S b
+	     "SQRT_ZZ_DAG" => Jabalizer.SQRT_ZZ_DAG,
+                 # H b; CNOT a b; H b; S a; S a; S a; S b; S b; S b
+             "SWAPCX" => Jabalizer.SWAPCX,
+                 # CNOT a b; CNOT b a
+             "XCX" => Jabalizer.XCX,
+                 # H a; CNOT a b; H a
+             "XCY" => Jabalizer.XCY,
+                 # H a; S b; S b; S b; CNOT a b; H a; S b
+             "XCZ" => Jabalizer.XCZ,
+                 # CNOT b a
+             "YCX" => Jabalizer.YCX,
+                 # S a; S a; S a; H b; CNOT b a; S a; H b
+             "YCY" => Jabalizer.YCY,
+                 # S a; S a; S a; S b; S b; S b; H a; CNOT a b; H a; S a; S b
+             "YCZ" => Jabalizer.YCZ,
+             # S a; S a; S a; CNOT b a; S a
+             ))
 end
 
 """
--- src/gates.jl
+++ src/gates.jl
@@ -1,5 +1,64 @@
 module Gates
 
+const gate_alias =
+    [("H_XZ", :H),
+     ("P", :S),
+     ("PHASE", :S),
+     ("SQRT_Z", :S),		# S
+     ("SQRT_Z_DAG", :S_DAG),	# SSS
+     ("CX", :CNOT),
+     ("ZCX", :CNOT),
+     ("ZCY", :CY),
+     ("ZCZ", :CZ),
+     ]
+
+const gate1_list =
+    [("X",     :X),	# HSSH
+     ("Y",     :Y),	# SSHSSH
+     ("Z",     :Z),	# SS
+     ("C_XYZ", :C_XYZ),	# SSSH
+     ("C_ZYX", :C_ZYX),	# HS
+     ("H",     :H),
+     ("H_XY",  :H_XY),	# HSSHS
+     ("H_YZ",  :H_YZ),	# HSHSS
+     ("S",     :S),
+     ("S_DAG", :S_DAG),
+     ("SQRT_X",     :SQRT_X),     # HSH
+     ("SQRT_X_DAG", :SQRT_X_DAG), # SHS
+     ("SQRT_Y",     :SQRT_Y),	  # SSH
+     ("SQRT_Y_DAG", :SQRT_Y_DAG), # HSS
+     ]
+
+const gate2_list =
+    [("CNOT",   :CNOT),
+     ("CY",     :CY),     # S1 S1 S1 CNOT01 S1
+     ("CZ",     :CZ),     # H1 CNOT01 H1
+     ("XCX",    :XCX),    # H0 CNOT01 H0
+     ("XCY",    :XCY),    # H0 S1 S1 S1 CNOT01 H0 S1
+     ("XCZ",    :XCZ),    # CNOT10
+     ("YCX",    :YCX),    # S0 S0 S0 H1 CNOT10 S0 H1
+     ("YCY",    :YCY),    # S0 S0 S0 S1 S1 S1 H0 CNOT01 H0 S0 S1
+     ("YCZ",    :YCZ),
+     ("SWAP",   :SWAP),   # CNOT01 CNOT10 CNOT01
+     ("CXSWAP", :CXSWAP), # CNOT10 CNOT01
+     ("SWAPCX", :SWAPCX), # CNOT01 CNOT10
+     ("ISWAP",  :ISWAP),  # H0 S0 S0 S0 CNOT01 S0 CNOT10 H1 S1 S0
+     ("ISWAP_DAG", :ISWAP_DAG),
+         # S0 S0 S0 S1 S1 S1 H1 CNOT10 S0 S0 S0 CNOT01 S0 H0
+     ("SQRT_XX", :SQRT_XX),
+         # H0 CNOT01 H1 S0 S1 H0 H1
+     ("SQRT_XX_DAG", :SQRT_XX_DAG),
+         # H0 CNOT01 H1 S0 S0 S0 S1 S1 S1 H0 H1
+     ("SQRT_YY", :SQRT_YY),
+         # S0 S0 S0 S1 S1 S1 H0 CNOT01 H1 S0 S1 H0 H1 S0 S1
+     ("SQRT_YY_DAG", :SQRT_YY_DAG),
+         # S0 S0 S0 S1 H0 CNOT01 H1 S0 S1 H0 H1 S0 S1 S1 S1
+     ("SQRT_ZZ", :SQRT_ZZ),
+         # H1 CNOT01 H1 S0 S1
+     ("SQRT_ZZ_DAG", :SQRT_ZZ_DAG),
+         # H1 CNOT01 H1 S0 S0 S0 S1 S1 S1
+     ]
+                   
 abstract type Gate end
 abstract type Gate1 <: Gate end
 abstract type Gate2 <: Gate end
@@ -7,10 +66,11 @@ abstract type Gate2 <: Gate end
 export Gate, Gate1, Gate2
 export Id, P, X, Y, Z, H, CNOT, CZ, SWAP
 
-"""P gate"""
-struct P <: Gate1 ; qubit::Int ; end
 """Pauli-I gate"""
 struct Id <: Gate1 ; qubit::Int ; end
+
+"""S (Phase) gate"""
+struct P <: Gate1 ; qubit::Int ; end
 """Pauli-X gate"""
 struct X <: Gate1 ; qubit::Int ; end
 """Pauli-Y gate"""
