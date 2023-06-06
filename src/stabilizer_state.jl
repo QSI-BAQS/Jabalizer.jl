26cb447a89a635211a6d9fe2c27ba1dc7fc58bfe
Madhav Krishnan Vijayan <39261345+madhavkrishnan@users.noreply.github.com>
2023-06-06 20:42:13 +1000

    gplot fix (#66)

--- src/stabilizer_state.jl
+++ src/stabilizer_state.jl
@@ -88,8 +88,8 @@ Plot the graph equivalent of a state.
 function GraphPlot.gplot(state::StabilizerState; node_dist=5.0)
     # Creates an anonymous function to allow changing the layout params
     # in gplot. The value of C determines distance between connected nodes.
-    layout = (args...) -> spring_layout(args...; C=node_dist)
-    gplot(Graph(GraphState(state).A), nodelabel=1:state.qubits, layout=layout)
+    layout = (args...) -> GraphPlot.spring_layout(args...; C=node_dist)
+    gplot(GraphPlot.Graph(GraphState(state).A), nodelabel=1:state.qubits, layout=layout)
 end
 
 
