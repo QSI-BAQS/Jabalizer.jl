aa0fb2c5df75ca2c7ae4e93f537026466e09fa13
Jannis Ruh <ruh.j81@gmail.com>
2023-11-10 14:22:24 +1100

    Update the pauli_tracker version
    The pauli_tracker library supports a bunch of more gates.

--- src/pauli_tracker.jl
+++ src/pauli_tracker.jl
@@ -17,7 +17,6 @@ function frames_new()::Ptr{Frames}
     @ccall lib.frames_hmpsbvfx_new()::Ptr{Frames}
 end
 
-
 function frames_free(frames::Ptr{Frames})::Cvoid
     @ccall lib.frames_hmpsbvfx_free(frames::Ptr{Frames})::Cvoid
 end
@@ -34,79 +33,88 @@ end
 function frames_track_x(frames::Ptr{Frames}, qubit::UInt)::Cvoid
     @ccall lib.frames_hmpsbvfx_track_x(frames::Ptr{Frames}, qubit::UInt)::Cvoid
 end
-
 function frames_track_y(frames::Ptr{Frames}, qubit::UInt)::Cvoid
     @ccall lib.frames_hmpsbvfx_track_y(frames::Ptr{Frames}, qubit::UInt)::Cvoid
 end
-
 function frames_track_z(frames::Ptr{Frames}, qubit::UInt)::Cvoid
     @ccall lib.frames_hmpsbvfx_track_z(frames::Ptr{Frames}, qubit::UInt)::Cvoid
 end
 
+function frames_id(_::Ptr{Frames}, _::UInt)::Cvoid end
+function frames_x(_::Ptr{Frames}, _::UInt)::Cvoid end
+function frames_y(_::Ptr{Frames}, _::UInt)::Cvoid end
+function frames_z(_::Ptr{Frames}, _::UInt)::Cvoid end
+function frames_s(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+    @ccall lib.frames_hmpsbvfx_s(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+end
+function frames_sdg(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+    @ccall lib.frames_hmpsbvfx_sdg(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+end
+function frames_sz(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+    @ccall lib.frames_hmpsbvfx_sz(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+end
+function frames_szdg(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+    @ccall lib.frames_hmpsbvfx_szdg(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+end
+function frames_hxy(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+    @ccall lib.frames_hmpsbvfx_hxy(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+end
 function frames_h(frames::Ptr{Frames}, qubit::UInt)::Cvoid
     @ccall lib.frames_hmpsbvfx_h(frames::Ptr{Frames}, qubit::UInt)::Cvoid
 end
-
-function frames_s(frames::Ptr{Frames}, qubit::UInt)::Cvoid
-    @ccall lib.frames_hmpsbvfx_s(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+function frames_sy(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+    @ccall lib.frames_hmpsbvfx_sy(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+end
+function frames_sydg(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+    @ccall lib.frames_hmpsbvfx_sydg(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+end
+function frames_sh(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+    @ccall lib.frames_hmpsbvfx_sh(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+end
+function frames_hs(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+    @ccall lib.frames_hmpsbvfx_hs(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+end
+function frames_shs(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+    @ccall lib.frames_hmpsbvfx_shs(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+end
+function frames_sx(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+    @ccall lib.frames_hmpsbvfx_sx(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+end
+function frames_sxdg(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+    @ccall lib.frames_hmpsbvfx_sxdg(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+end
+function frames_hyz(frames::Ptr{Frames}, qubit::UInt)::Cvoid
+    @ccall lib.frames_hmpsbvfx_hyz(frames::Ptr{Frames}, qubit::UInt)::Cvoid
 end
 
 function frames_cz(frames::Ptr{Frames}, qubit_a::UInt, qubit_b::UInt)::Cvoid
     @ccall lib.frames_hmpsbvfx_cz(frames::Ptr{Frames}, qubit_a::UInt, qubit_b::UInt)::Cvoid
 end
-
-function frames_x(_::Ptr{Frames}, _::UInt)::Cvoid end
-function frames_y(_::Ptr{Frames}, _::UInt)::Cvoid end
-function frames_z(_::Ptr{Frames}, _::UInt)::Cvoid end
-
-function frames_sdg(frames::Ptr{Frames}, qubit::UInt)::Cvoid
-    @ccall lib.frames_hmpsbvfx_sdg(frames::Ptr{Frames}, qubit::UInt)::Cvoid
-end
-
-function frames_sx(frames::Ptr{Frames}, qubit::UInt)::Cvoid
-    @ccall lib.frames_hmpsbvfx_sx(frames::Ptr{Frames}, qubit::UInt)::Cvoid
-end
-
-function frames_sxdg(frames::Ptr{Frames}, qubit::UInt)::Cvoid
-    @ccall lib.frames_hmpsbvfx_sxdg(frames::Ptr{Frames}, qubit::UInt)::Cvoid
-end
-
-function frames_sy(frames::Ptr{Frames}, qubit::UInt)::Cvoid
-    @ccall lib.frames_hmpsbvfx_sy(frames::Ptr{Frames}, qubit::UInt)::Cvoid
-end
-
-function frames_sydg(frames::Ptr{Frames}, qubit::UInt)::Cvoid
-    @ccall lib.frames_hmpsbvfx_sydg(frames::Ptr{Frames}, qubit::UInt)::Cvoid
-end
-
-function frames_sz(frames::Ptr{Frames}, qubit::UInt)::Cvoid
-    @ccall lib.frames_hmpsbvfx_sz(frames::Ptr{Frames}, qubit::UInt)::Cvoid
-end
-
-function frames_szdg(frames::Ptr{Frames}, qubit::UInt)::Cvoid
-    @ccall lib.frames_hmpsbvfx_szdg(frames::Ptr{Frames}, qubit::UInt)::Cvoid
-end
-
 function frames_cx(frames::Ptr{Frames}, control::UInt, target::UInt)::Cvoid
     @ccall lib.frames_hmpsbvfx_cx(frames::Ptr{Frames}, control::UInt, target::UInt)::Cvoid
 end
-
+function frames_cy(frames::Ptr{Frames}, control::UInt, target::UInt)::Cvoid
+    @ccall lib.frames_hmpsbvfx_cy(frames::Ptr{Frames}, control::UInt, target::UInt)::Cvoid
+end
 function frames_swap(frames::Ptr{Frames}, qubit_a::UInt, qubit_b::UInt)::Cvoid
     @ccall lib.frames_hmpsbvfx_swap(frames::Ptr{Frames}, qubit_a::UInt, qubit_b::UInt)::Cvoid
+end
+function frames_iswap(frames::Ptr{Frames}, qubit_a::UInt, qubit_b::UInt)::Cvoid
+    @ccall lib.frames_hmpsbvfx_iswap(frames::Ptr{Frames}, qubit_a::UInt, qubit_b::UInt)::Cvoid
+end
+function frames_iswapdg(frames::Ptr{Frames}, qubit_a::UInt, qubit_b::UInt)::Cvoid
+    @ccall lib.frames_hmpsbvfx_iswapdg(frames::Ptr{Frames}, qubit_a::UInt, qubit_b::UInt)::Cvoid
 end
 
 function frames_move_x_to_x(frames::Ptr{Frames}, origin::UInt, new::UInt)::Cvoid
     @ccall lib.frames_hmpsbvfx_move_x_to_x(frames::Ptr{Frames}, origin::UInt, new::UInt)::Cvoid
 end
-
 function frames_move_x_to_z(frames::Ptr{Frames}, origin::UInt, new::UInt)::Cvoid
     @ccall lib.frames_hmpsbvfx_move_x_to_z(frames::Ptr{Frames}, origin::UInt, new::UInt)::Cvoid
 end
-
 function frames_move_z_to_x(frames::Ptr{Frames}, origin::UInt, new::UInt)::Cvoid
     @ccall lib.frames_hmpsbvfx_move_z_to_x(frames::Ptr{Frames}, origin::UInt, new::UInt)::Cvoid
 end
-
 function frames_move_z_to_z(frames::Ptr{Frames}, origin::UInt, new::UInt)::Cvoid
     @ccall lib.frames_hmpsbvfx_move_z_to_z(frames::Ptr{Frames}, origin::UInt, new::UInt)::Cvoid
 end
