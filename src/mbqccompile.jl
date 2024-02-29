370a73015d39c2a3602d3a65d16fe87ba0f3b23b
thinh-le <thinh.phuc.le@gmail.com>
2024-02-29 18:37:14 +1100

    wip fixing segfault

--- src/mbqccompile.jl
+++ src/mbqccompile.jl
@@ -1,5 +1,6 @@
 using Graphs
 using PythonCall
+using JSON
 export mbqccompile
 
 mbqc_scheduling = pyimport("mbqc_scheduling")
@@ -11,10 +12,10 @@ PartialOrderGraph = pyimport("mbqc_scheduling").PartialOrderGraph
 """
 function mbqccompile(
     circuit::QuantumCircuit;
-    universal::Bool = true,
-    ptracking::Bool = true,
-    teleport        = ["T", "T_Dagger", "RZ"]
-    filepath::String=nothing,
+    universal = true,
+    ptracking = true,
+    teleport  = ["T", "T_Dagger", "RZ"],
+    filepath  = nothing,
 )
     icm, measure, labels, ptracker = icmcompile(circuit; universal=universal, ptracking = ptracking, teleport = teleport)
     state = zero_state(width(icm))
@@ -26,12 +27,33 @@ function mbqccompile(
     for (s, i) in zip(labels[:state], labels[:input]) # teleporting state into input registers
         add_edge!(fullgraph, s, i)
     end # extended graph state with state registers for scheduling
-    spacialgraph = SpacialGraph([nb .- 1 for nb in SimpleGraphs.adj(fullgraph)]) # zero-based indexing
-    ptracking && (order = PartialOrderGraph(ptracker[:frames].get_py_order(ptracker[:frameflags])))
-    AcceptFunc = pyimport("mbqc_scheduling.probabilistic").AcceptFunc
-    paths = mbqc_scheduling.run(spacialgraph, order)
-    for path in paths.into_py_paths()
-        @info "time: $(path.time); space: $(path.space); steps: $(path.steps)"
-    end
+    # SEGFAULT ???
+    spacialgraph = SpacialGraph([nb .- 1 for nb in Graphs.SimpleGraphs.adj(fullgraph)]) # zero-based indexing
+    # ptracking && (order = PartialOrderGraph(ptracker[:frames].get_py_order(ptracker[:frameflags])))
+    # AcceptFunc = pyimport("mbqc_scheduling.probabilistic").AcceptFunc
+    # paths = mbqc_scheduling.run(spacialgraph, order)
+    # path0 = paths.into_py_paths()[0]
+    # ttime = pyconvert(Int, path0.time)
+    # space = pyconvert(Int, path0.space)
+    # steps = [pyconvert(Vector, step) for step in path0.steps]
     # Jabalizer output
+    # measurements = append!(measure, Gate("X", nothing, [i]) for i in labels[:state])
+    # jabalizer_out = Dict(
+    #     :time => ttime,
+    #     :space => space,
+    #     :steps => steps,
+    #     :correction => correction,
+    #     :measurements => map(unpackGate, measurements),
+    # )
+    # if !nothing(filepath)
+    #     @info "Jabalizer: writing to $(filepath)"
+    #     open(filepath, "w") do f
+    #         write(f, JSON.json(jabalizer_out))
+    #     end
+    # end
+    return jabalizer_out
+end
+
+function unpackGate(gate::Gate)
+    return (Jabalizer.name(gate), Jabalizer.qargs(gate), Jabalizer.cargs(gate))
 end
