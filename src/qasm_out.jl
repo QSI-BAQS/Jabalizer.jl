7674c690e52d8a4c79d546dd493db32de45fa8e9
Madhav Vijayan <39261345+madhavkrishnan@users.noreply.github.com>
2024-03-28 02:45:55 +0530

    wip: add mbqc instruction input method to qasm_instruction

--- src/qasm_out.jl
+++ src/qasm_out.jl
@@ -63,7 +63,12 @@ end
 """
 export qasm file given a graph compilation
 """
-function qasm_instruction(outfile, graph, loc_corr, mseq, data_qubits, frames_array)
+function qasm_instruction(outfile, 
+                          graph::SimpleGraph{Int},
+                          loc_corr,
+                          mseq,
+                          data_qubits,
+                          frames_array)
     
     # open output file
     file = open(outfile, "w")
@@ -178,4 +183,144 @@ function qasm_instruction(outfile, graph, loc_corr, mseq, data_qubits, frames_ar
     end
 
     return nothing
+end
+
+"""
+Generates a qasm file implementing the mbqc instruction
+"""
+function qasm_instruction(outfile,
+                          mbqc_inst::Dict)
+
+    # open output file
+    file = open(outfile, "w")
+    
+    # unpack data dictionary
+    qtot = mbqc_inst[:space]
+    mlayers = mbqc_inst[:steps]
+    pc = mbqc_inst[:pcorrs]
+    outqubits = mbqc_inst[:outputnodes]
+    statequbits = mbqc_inst[:statenodes]
+    loc_corr = mbqc_inst[:correction]
+    measurements = mbqc_inst[:measurements]
+    graph = mbqc_inst[:spatialgraph]
+
+    # initialise map to keep track of qubits
+    qmap = Dict()
+    # Convert measurement sequence to a dictionary. 
+    # the sequence is now decided by layers
+    mdict = Dict()
+    for m in measurements
+    mdict[m[2]] = (m[1], m[3]) 
+    end
+
+    # initialise qasm file
+    println(file, "OPENQASM 2.0;\ninclude \"qelib1.inc\";" )
+   
+    # # initialise qregs and cregs
+    # qregs = nv(graph)
+    # qregs_total = qregs + length(data_qubits[:state])
+    qlogical = length(measurements) + length(outqubits)
+    println(file, "\nqreg q[$qlogical];")
+
+    # defining a classical register for every logical qubit 
+    for i in 0:qlogical-1
+        println(file, "creg c$(i)[1];")
+    end
+
+    # Generate and measure layers
+    # keep track of initialised qubits
+    init = copy(statequbits)
+    # keep track of qubit connections
+    connection_graph = SimpleGraph(qlogical)
+    for lr in mlayers
+        # initialise qubits and neighbors if not initialised
+        for q in lr
+            if !(q in init)
+                println(file, "h q[$q];")
+                push!(init, q)
+            end
+            for n in graph[q+1]
+                if !(n in init)
+                    println(file, "h q[$n];")
+                    push!(init, n)
+                end
+            end
+
+        end
+        # conect all neighbors of qubits in measurement layer
+        for q in lr
+            for n in graph[q+1]
+                # check if already connected
+                if !(n+1 in neighbors(connection_graph, q+1))
+                    println(file, "cz q[$q], q[$n];")
+                    add_edge!(connection_graph, q+1, n+1) 
+                end
+            end
+        end
+
+        # apply local corrections
+
+        for gate in loc_corr
+            if gate[2] in lr
+                if gate[1] == "H"
+                    println(file, "h q[$(gate[2])];")
+                end
+        
+                if gate[1] == "Z"
+                    println(file, "z q[$(gate[2])];")
+                end
+        
+                # inverting local clifford corrections applies the inverse gate
+                if gate[1] == "Pdag"
+                    println(file, "s q[$(gate[2])];")
+                end
+            end
+        end
+
+        # measurement
+        for q in lr
+            # apply Pauli corrections.
+            if haskey(pc, q)
+                for p in pc[q]
+                    println(file, "if(c$(p[2]) == 1) $(p[1]) q[$q];")
+                end
+            end
+
+            # skip measurement for output qubits
+            (q in outqubits) && continue
+            # perform measurements
+            gate = qasm_inversemap[mdict[q][1]]
+
+            if gate == "x"
+                println(file, "h q[$q];")
+                println(file, "measure q[$q] -> c$q[0];")
+            elseif gate in ["t", "tdg"]
+                println(file, "$gate q[$q];" )
+                println(file, "h q[$q];")
+                println(file, "measure q[$q] -> c$q[0];")
+            elseif gate == "rz"
+                @info mdict[q]
+                println(file, "$gate($(mdict[q][2])) q[$q];" )
+                println(file, "h q[$q];")
+                println(file, "measure q[$q] -> c$q[0];")
+            end
+
+        end
+
+
+
+
+    end 
+
+    close(file)
+    data_qubits = Dict()
+    data_qubits[:state] = statequbits
+    data_qubits[:output] = outqubits
+
+    data_file = splitext(outfile)[1]*"_dqubits.json"
+    open(data_file,"w") do f
+        JSON.print(f, data_qubits)
+    end
+
+    return nothing    
 end
